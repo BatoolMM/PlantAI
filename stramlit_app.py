9341203cb39aa1e7732601931e45f49b51c58430
Batool <53487593+BatoolMM@users.noreply.github.com>
2020-09-24 07:16:53 +0300

    added files for streamlit app

---
+++ stramlit_app.py
@@ -0,0 +1,37 @@
+
+from google.colab import drive
+from fastai.vision.all import *
+from fastai.vision.widgets import *
+from IPython.display import Image
+import virtualbox
+from ipywidgets import * 
+from PIL import Image
+import streamlit as st
+from pyngrok import ngrok
+import streamlit_theme as stt
+
+
+path = Path()
+stt.set_theme({'primary': '#1b3388'})
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.title("PlantAI")
+st.write('''
+  ---
+  ## **Which Plant is This**?
+   This is simple Web application to classify the type of the plants I have in my home and provide instruction of taking care of it.
+  ### Please ask me anything!  
+''')
+Uploaded = st.file_uploader('', type=['png','jpg','jpeg'])
+
+with st.spinner('Loading your picture into memory'):
+  model = load_learner('/content/export.pkl')
+  time.sleep(1)
+
+if Uploaded is not None:
+    img = Image.open(Uploaded)
+    img_fastai = PILImage.create(Uploaded)
+    st.image(img, caption='This is your uploaded picture', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    pred,pred_idx,probs = model.predict(img_fastai)
+    st.write("Prediction: ", pred, "; Probability: ", probs[pred_idx]*100,'%')
