76ea74f4eb6d550b57a1abcfc3a9408e1087cd8a
Batool <53487593+BatoolMM@users.noreply.github.com>
2020-09-26 06:13:26 +0300

    upadated app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 
-from fastai.vision import open_image, load_learner, image, torch
+from fastai.vision.all import *
 from fastai.vision.widgets import *
+from IPython.display import Image
+from ipywidgets import * 
 from PIL import Image
 import streamlit as st
-import time
+from pyngrok import ngrok
+import urllib.request
+import wget
 
 path = Path()
 st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -11,13 +15,22 @@ st.title("PlantAI")
 st.write('''
   ---
   ## **Which Plant is This**?
-   This is simple Web application to classify the type of the plants I have in my home and provide instruction of taking care of it.
-  ### Please ask me anything!  
+   This is simple Web application to classify four type of plants:
+   **Schefflera**, 
+   **Epipremnum aureum**,
+   **Dieffenbachia**,
+   **Dracaena**.
+'''
+)
+st.image('https://dl.dropboxusercontent.com/s/2873nwxz55lrozc/plants.jpg?dl=0', use_column_width=True)
+st.write('''
+  ### Please upload a picture for one of these plants!  
 ''')
 Uploaded = st.file_uploader('', type=['png','jpg','jpeg'])
 
 with st.spinner('Loading your picture into memory'):
-  model = load_learner(path'/export.pkl')
+  url = wget.download('https://dl.dropboxusercontent.com/s/uahengnua8m3hbu/export.pkl?dl=0')
+  model = load_learner(url)
   time.sleep(1)
 
 if Uploaded is not None:
@@ -27,4 +40,5 @@ if Uploaded is not None:
     st.write("")
     st.write("Classifying...")
     pred,pred_idx,probs = model.predict(img_fastai)
-    st.write("Prediction: ", pred, "; Probability: ", probs[pred_idx]*100,'%')
+    st.write("Prediction: ", pred, "; Probability: ", probs[pred_idx]*100,'%  :sunglasses:')
+
